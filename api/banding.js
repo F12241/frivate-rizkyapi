0c57b0858843d863ee64dda2b1a803a22a5410ed
F12241 <andi081287@gmail.com>
2025-11-29 20:13:09 +0700

    Update banding.js

--- api/banding.js
+++ api/banding.js
@@ -111,7 +111,6 @@ Nomor saya (+${nomor}).`
     res.json({
       success: true,
       message: 'Email banding berhasil dikirim',
-      usedAccount: account.user,
       nomor: `+${nomor}`,
       messageId: info.messageId
     })
